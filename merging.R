12ccc50e13ca8d340bfd3760aa154bc71cbcf6d1
Samantha <samanthakamath2000@gmail.com>
2019-07-08 14:28:18 -0400

    Updated script
    Put back into the format of the NC data

--- merging.R
+++ merging.R
@@ -8,13 +8,21 @@ datasets = list(sqf03mod,sqf04mod,sqf05mod,sqf06mod,sqf07mod,sqf08mod,sqf09mod,
 crimes = read.csv(file = "precinct_crime_data.csv", stringsAsFactors = F)
 for (i in 1: length(datasets))
 {
-  df = merge(datasets[[i]] %>% filter(race == "B"), datasets[[i]] 
-             %>% filter(race == "H"), by = "pct", all = TRUE)
-  datasets[[i]] = merge(df, datasets[[i]] %>% filter(race == "W"), by = "pct", all = TRUE)
+  datasets[[i]] = merge(datasets[[i]] %>% filter(race == "B"), datasets[[i]]
+             %>% filter(race == "H"), by = "pct", all = TRUE) %>%
+             merge(datasets[[i]] %>% filter(race == "W"), by = "pct", all = TRUE)
+  datasets[[i]]$race[is.na(datasets[[i]]$race)] = "W"
+  datasets[[i]][is.na(datasets[[i]])] = 0
+  names(datasets[[i]]) = c("pct", "race", "numstops", "numsearches", "numhits", "searchrate", 
+                 "hitrate", "race", "numstops", "numsearches", "numhits", "searchrate",
+                 "hitrate", "race", "numstops", "numsearches", "numhits", "searchrate",
+                 "hitrate")
+  datasets[[i]] = merge(datasets[[i]][,c(1,2:7)], datasets[[i]][,c(1,8:13)], all = TRUE) %>%
+       merge(datasets[[i]][,c(1,14:19)], all = TRUE)
 }
 for (i in 1: length(datasets))
 {
-  datasets[[i]]$numcrimes = crimes[,i+1] %>% na.omit()
+  datasets[[i]]$numcrimes = rep(crimes[,i+1] %>% na.omit(), each = 3, times = 1)
 }
 
 sqf03mod = datasets[[1]]
@@ -34,22 +42,15 @@ sqf16mod = datasets[[14]]
 sqf17mod = datasets[[16]]
 sqf18mod = datasets[[16]]
 
-#merges a list of 5 dataframes. to include more dataframes, add them in the below line
-library(reshapmd2)
-md = rbind(melt(datasets[[1]],id=colnames(datasets[[1]])), 
+#merges a list of 5 dataframes. to include more dataframes, add them in line 39-43
+library(reshape2)
+md = rbind(melt(datasets[[1]],id=colnames(datasets[[1]])),
           melt(datasets[[2]],id=colnames(datasets[[2]])),
           melt(datasets[[3]],id=colnames(datasets[[3]])),
           melt(datasets[[4]],id=colnames(datasets[[4]])),
-          melt(datasets[[5]],id=colnames(datasets[[5]])))
-md1 = ddply(md[1:7],c("pct","race.x"),numcolwise(sum))
-md1$searchrate.x = md1$numsearches.x/md1$numstops.x
-md1$hitrate.x = md1$numhits.x/md1$numstops.x
-md2 = ddply(md[c(1,8:13)],c("pct","race.y"),numcolwise(sum))
-md2$searchrate.y = md2$numsearches.y/md2$numstops.y
-md2$hitrate.y = md2$numhits.y/md2$numstops.y
-md3 = ddply(md[c(1,14:20)],c("pct","race"),numcolwise(sum))
-md3$searchrate = md3$numsearches/md3$numstops
-md3$hitrate = md3$numhits/md3$numstops
-md = merge(md1, md2) %>% merge(md3)
+          melt(datasets[[5]],id=colnames(datasets[[5]]))) %>%
+          ddply(c("pct","race"),numcolwise(sum))
+md$searchrate = md1$numsearches/md1$numstops
+md$hitrate = md$numhits/md$numstops
 
 write.csv(md, file = "mergeddata.csv")
